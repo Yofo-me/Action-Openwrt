27ae8138509d7687fc2748d26de3987d45c614c7
yoki <55388087+Yofo-me@users.noreply.github.com>
2023-03-13 16:38:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,36 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+cd package/lean/ && git clone -b js https://github.com/gngpp/luci-theme-design.git
+if [ ! -d package/lean/sms-tool ]
+then
+  mkdir package/lean/sms-tool
+fi
+
+if [ ! -d package/lean/luci-app-sms ]
+then
+  mkdir package/lean/sms-tool
+fi
+
+if [ ! -d package/lean/luci-app-passwall2 ]
+then
+  mkdir package/lean/sms-tool
+fi
+
+# for sms-tool
+cd package/lean/sms-tool && git init && git remote add -f origin https://github.com/kenzok8/small-package.git && git config core.sparsecheckout true && echo "sms-tool/*" >> .git/info/sparse-checkout && git pull origin main
+
+# for sms-app
+cd package/lean/luci-sms-app && git init && git remote add -f origin https://github.com/kenzok8/small-package.git && git config core.sparsecheckout true &&   echo "luci-app-sms-tool/*" >> .git/info/sparse-checkout && git pull origin main
+
+# for passwall2
+cd package/lean/luci-app-passwall2 && git init && git remote add -f origin https://github.com/kenzok8/small-package.git && git config core.sparsecheckout true &&  echo "luci-app-passwall2/*" >> .git/info/sparse-checkout && git pull origin main
+# modify passwall2 deps
+if [ -f package/lean/luci-app-passwall2/Makefile ]
+then
+  sed -i '31s/xray-core/v2ray-core/' package/lean/luci-app-passwall2/Makefile
+fi
+
+
