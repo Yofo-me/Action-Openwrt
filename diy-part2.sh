c63a9af9ad81f035be9ef5d18878466deb1fb4b1
yoki <55388087+Yofo-me@users.noreply.github.com>
2023-03-14 10:03:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,20 +15,14 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 cd package/lean/ && git clone -b js https://github.com/gngpp/luci-theme-design.git
 echo $PWD
-if [ ! -d package/lean/sms-tool ]
-then
-  mkdir package/lean/sms-tool
-fi
+[ ! -d package/lean/sms-tool ] && mkdir package/lean/sms-tool
 
-if [ ! -d package/lean/luci-app-sms ]
-then
-  mkdir package/lean/sms-tool
-fi
 
-if [ ! -d package/lean/luci-app-passwall2 ]
-then
-  mkdir package/lean/sms-tool
-fi
+[ ! -d package/lean/luci-app-sms ] && mkdir package/lean/sms-tool
+
+
+[ ! -d package/lean/luci-app-passwall2 ] && mkdir package/lean/sms-tool
+
 
 # for sms-tool
 if [  -d package/lean/sms-tool ]
@@ -49,16 +43,13 @@ cd package/lean/luci-app-passwall2 && git init && git remote add -f origin https
 fi
 
 # modify passwall2 deps
-if [ -f package/lean/luci-app-passwall2/Makefile ]
-then
-  sed -i '31s/xray-core/v2ray-core/' package/lean/luci-app-passwall2/Makefile
-fi
+[ -f package/lean/luci-app-passwall2/Makefile ] && sed -i '31s/xray-core/v2ray-core/' package/lean/luci-app-passwall2/Makefile
+
 
 #modify netdata
-sed -i 's/disable-https/enable-https/' package/feeds/packages/netdata/Makefile
-sed -i 's/disable-plugin/enable-plugin/' package/feeds/packages/netdata/Makefile
+[ -f package/feeds/packages/netdata/Makefile ] && sed -i 's/disable-https/enable-https/' package/feeds/packages/netdata/Makefile
+[ -f feeds/packages/admin/netdata/Makefile ] && sed -i 's/disable-plugin/enable-plugin/' feeds/packages/admin/netdata/Makefile
 
 #modify ttyd to enable https
-[ -f feeds/packages/utils/ttyd/Makefile ] && $(sed -i 's/PKG_VERSION:=1\.6\.3/PKG_VERSION:=1\.7\.3/' feeds/packages/utils/ttyd/Makefile && sed -i 's/PKG_HASH:=.*/PKG_HASH:=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/' feeds/packages/utils/ttyd/Makefile) || echo 'Files not Found'
-
-[ -f package/feeds/packages/ttyd/Makefile ] && $(sed -i 's/PKG_VERSION:=1\.6\.3/PKG_VERSION:=1\.7\.3/' package/feeds/packages/ttyd/Makefile && sed -i 's/PKG_HASH:=.*/PKG_HASH:=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/' package/feeds/packages/ttyd/Makefile) || echo 'Files not Found'
+[ -f feeds/packages/utils/ttyd/Makefile ] && $(sed -i 's/PKG_VERSION:=1\.6\.3/PKG_VERSION:=1\.7\.3/' feeds/packages/utils/ttyd/Makefile && sed -i 's/PKG_HASH:=.*/PKG_HASH:=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/' feeds/packages/utils/ttyd/Makefile) 
+[ -f package/feeds/packages/ttyd/Makefile ] && $(sed -i 's/PKG_VERSION:=1\.6\.3/PKG_VERSION:=1\.7\.3/' package/feeds/packages/ttyd/Makefile && sed -i 's/PKG_HASH:=.*/PKG_HASH:=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/' package/feeds/packages/ttyd/Makefile) 
